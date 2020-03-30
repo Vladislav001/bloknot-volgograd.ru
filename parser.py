6d4eac9cbbf097ff9553ba7e5c4cc03647cfaf55
Vladislav001 <VladGurjev@mail.ru>
2020-03-30 12:11:30 +0400

    Наработки по парсеру

---
+++ parser.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+
+# class News(object):
+#     def __init__(self, name):
+#         self.name = name
+
+siteUrl = 'https://bloknot-volgograd.ru'
+nextPage = ''
+
+for number in range(1):
+
+    # главная или нет
+    if nextPage == '':
+        newsListUrl = 'https://bloknot-volgograd.ru/'
+    else:
+        newsListUrl = 'https://bloknot-volgograd.ru' + nextPage
+
+    newsListUrl = requests.get(newsListUrl)
+
+    # страница
+    soup = BeautifulSoup(newsListUrl.text, "html.parser")
+
+    # получить адрес следующей страницы пагинации
+    nextPage = soup.find(id='navigation_1_next_page').get('href')
+
+    # собрать новости на текущей странице
+    newsList = soup.find('ul', class_='bigline').findAll('a', class_='sys')
+
+    # обработать данные о каждой новости
+    for i in range(len(newsList)):
+        # ссылка на новость
+        currentNewsUrl = siteUrl + newsList[i].get('href')
+
+        # поля новости
+        newsData = requests.get(currentNewsUrl)
+        soupNews = BeautifulSoup(newsData.text, "html.parser")
+        name = soupNews.find('h1').text
+        print(name)
+
+        # newsObject = News(name)
+        # print(newsObject)
