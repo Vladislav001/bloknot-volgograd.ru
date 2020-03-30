1b2c1fab7de23aafa705da6738ee0526b64fe726
Vladislav001 <VladGurjev@mail.ru>
2020-03-30 15:44:01 +0400

    Вынес парсинг в класс

--- parser.py
+++ parser.py
@@ -2,45 +2,46 @@ from bs4 import BeautifulSoup
 from database import Database
 import requests
 
-# class News(object):
-#     def __init__(self, name):
-#         self.name = name
+class Parser:
+    siteUrl = 'https://bloknot-volgograd.ru'
+    nextPage = ''
 
-siteUrl = 'https://bloknot-volgograd.ru'
-nextPage = ''
+    def doParsing(self):
+        for number in range(2):
 
-for number in range(1):
+            # главная или нет
+            if self.nextPage == '':
+                newsListUrl = 'https://bloknot-volgograd.ru/'
+            else:
+                newsListUrl = 'https://bloknot-volgograd.ru' + self.nextPage
 
-    # главная или нет
-    if nextPage == '':
-        newsListUrl = 'https://bloknot-volgograd.ru/'
-    else:
-        newsListUrl = 'https://bloknot-volgograd.ru' + nextPage
+            newsListUrl = requests.get(newsListUrl)
 
-    newsListUrl = requests.get(newsListUrl)
+            # страница
+            soup = BeautifulSoup(newsListUrl.text, "html.parser")
 
-    # страница
-    soup = BeautifulSoup(newsListUrl.text, "html.parser")
+            # получить адрес следующей страницы пагинации
+            nextPage = soup.find(id='navigation_1_next_page').get('href')
 
-    # получить адрес следующей страницы пагинации
-    nextPage = soup.find(id='navigation_1_next_page').get('href')
+            # собрать новости на текущей странице
+            newsList = soup.find('ul', class_='bigline').findAll('a', class_='sys')
 
-    # собрать новости на текущей странице
-    newsList = soup.find('ul', class_='bigline').findAll('a', class_='sys')
+            # обработать данные о каждой новости
+            for i in range(len(newsList)):
+                # ссылка на новость
+                currentNewsUrl = self.siteUrl + newsList[i].get('href')
 
-    # обработать данные о каждой новости
-    for i in range(len(newsList)):
-        # ссылка на новость
-        currentNewsUrl = siteUrl + newsList[i].get('href')
+                # поля новости
+                newsData = requests.get(currentNewsUrl)
+                soupNews = BeautifulSoup(newsData.text, "html.parser")
+                name = soupNews.find('h1').text
+                print(name)
 
-        # поля новости
-        newsData = requests.get(currentNewsUrl)
-        soupNews = BeautifulSoup(newsData.text, "html.parser")
-        name = soupNews.find('h1').text
-        print(name)
+                # newsObject = News(name)
+                # print(newsObject)
 
-        # newsObject = News(name)
-        # print(newsObject)
+parser = Parser()
+parser.doParsing()
 
-database = Database()
-database.addRecord( {"footg" : "bagggr" })
+# database = Database()
+# database.addRecord( {"footg" : "bagggr" })
