0e9da9c5264413614c3a61535b2d13dfeade58d1
Vladislav001 <VladGurjev@mail.ru>
2020-03-31 14:50:55 +0400

    Запись даты

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from database import Database
+from datetime import datetime, timedelta
 import requests
 
 class Parser:
@@ -12,9 +13,9 @@ class Parser:
         # узнать общее кол-во страниц пагинации
         first = requests.get(self.siteUrl)
         soupFirst = BeautifulSoup(first.text, "html.parser")
-        lastPageNumber = int(soupFirst.find('div', class_='navigation-pages').findAll('a')[-1].text)
+        lastPageNumber = int(soupFirst.find('div', class_='navigation-pages').findAll('a')[-1].text) # для for page in range(1):
 
-        for page in range(1):
+        for page in range(8):
             #print(page + 1)
             # главная или нет
             if self.nextPage == '':
@@ -31,7 +32,7 @@ class Parser:
             self.nextPage = soupNewsList.find(id='navigation_1_next_page').get('href')
 
             # собрать новости на текущей странице
-            newsListArr = soupNewsList.find('ul', class_='bigline').findAll('a', class_='sys')
+            newsListArr = soupNewsList.find('ul', class_='bigline').findAll('a', class_='sys') # для for i in range
 
             # обработать данные о каждой новости
             for i in range(len(newsListArr)):
@@ -51,24 +52,31 @@ class Parser:
                 name = soupNews.find('h1').text
                 date = soupNewsList.find('a', href=currentNewsShortUrl + '#comments').find_parent('span').next_element.next_element
                 # обработаем дату
-                date = self.converteDate(date)
+                date = str(self.converteDate(date))
 
                 href = currentNewsFullUrl
                 text = soupNews.find('div', class_='news-text').text
                 count_comments = int(soupNewsList.find('a', href=currentNewsShortUrl + '#comments').text)
-                print(name)
+
                 # запись в БД
                 database.addRecord({
                     "name" : name,
-                    #"date" : date,
+                    "date" : date,
                     "href" : href,
                     "text" : text,
                     "count_comments" : count_comments
                 })
 
     def converteDate(self, date):
-        newDate = date
-        #TODO чекать: менять сегодня, вчера
+        if "сегодня" in date:
+            newDate = datetime.now().date()
+        elif "вчера" in date:
+            newDate = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+        else:
+            date = ''.join(date.split())
+            newDate = datetime.strptime(date, '%d.%m.%Y')
+            newDate = datetime.strftime(newDate, '%Y-%m-%d')
+
         return newDate
 
 parser = Parser()
