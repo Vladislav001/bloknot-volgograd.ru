b4c04a5785f12424bae1461b9eea8ae3857feb24
Vladislav001 <VladGurjev@mail.ru>
2020-03-31 18:13:32 +0400

    Наработки по вебу

--- server.py
+++ server.py
@@ -1,4 +1,18 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-server_address = ("", 8000)
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+from flask import Flask, render_template
+from database import Database
+
+database = Database()
+allRecords = database.getAllRecords()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', records=allRecords)
+
+@app.route('/main')
+def main():
+    return 'main'
+
+if __name__ == "__main__":
+    app.run()
